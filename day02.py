86966646aba4b5b3bc616ac29aed9c60df79583a
DESKTOP-DP2C5J4\USER <siko2828@naver.com>
2025-02-11 17:07:07 +0900

    v1.1

--- day02.py
+++ day02.py
@@ -5,13 +5,17 @@ def is_prime(num)->bool: #타입힌트
     :return:
     """
     if num >= 2:
-        for i in range(2,int(num**0.5)+1):
-            if num%i ==0:
+        i=2
+        while i<(int(num**0.5)+1):
+
+            if num%i==0:
                 return False
+            i+=1
 
     else:
         return False
     return True
+
 help(is_prime) #함수에 대한 설명 출력
 n=int(input("input number : "))
 
