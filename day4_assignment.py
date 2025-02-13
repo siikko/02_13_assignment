96913d924bf07026afc1349f8230f3d9ddc91ffa
DESKTOP-DP2C5J4\USER <siko2828@naver.com>
2025-02-13 17:35:55 +0900

    assignment

--- day4_assignment.py
+++ day4_assignment.py
@@ -1,53 +1,18 @@
-# SOLID
-# Open Closed Principle : 개방 폐쇄 원칙 (확장에는 열려 있고 수정에는 닫혀있는 원칙)
-#2중 데코레이터 적용, 성능측정 데코레이터, 디스크립션 데코레이터를 팩토리얼 함수에 적용하시오
-import time
-
-
-#교수님코드
-def description_decorator(func):
-    def wrapper(*arg):
-        print(func.__name__)
-        print(func.__doc__)
-        r=func(*arg)
-        return r
-    return wrapper #괄호없어야함.함수호출아님
-
-
-
-def time_decorator(func):
-    def wrapper(*arg):
-        s = time.time()
-        r = func(*arg)
-        e = time.time()
-        print(f'실행시간 : {e - s}초')
-        return r
-    return wrapper
-
-
-def description_decorator(func):
-    def wrapper(*args, **kwargs):
-        print(f"함수 `{func.__name__}`을 실행합니다.")
-        return func(*args, **kwargs)
-    return wrapper
-
-
-@time_decorator
-@description_decorator
-def factorial_repetition(n) -> int:
-    """
-    factorial fucntion by loop
-    :param n:
-    :return: results of factorial operation
-    """
-    result = 1
-    for i in range(2, n+1):
-        result = result * i
-    return result
-
-
-# number = int(input())
-# ft = time_decorator(factorial_repetition)
-# print(f"{number}! = {ft(number)}")
-number = int(input())
-print(f"{number}! = {factorial_repetition(number)}")
+#assingment
+# v3.8) kw
+def log_decorator(func):
+    def wrapper(*args,**kwargs):
+        print(f'Function Name : {func.__name__}')
+        print(f'Function Arguments : {args}')
+        print(f'Function keyword Arguments : {kwargs}')
+        result=func(*args,**kwargs)
+        return result
+    return wrapper #항상정해져있음
+@log_decorator
+def greet(name, greeting="안녕하세요",age=None):
+    return f"{greeting}, {name}(age:{age})"if age else f"{greeting}, {name}"
+print(greet("인하"))
+print(greet("인상","안녕"))
+print(greet("James","Hello"))
+print(greet("Gonzales",greeting="Hola"))
+print(greet("Nakamura",greeting="Gonniziwa",age=29))
